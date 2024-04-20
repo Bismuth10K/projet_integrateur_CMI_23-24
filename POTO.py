c2b0db07df28478fcd18e53a6b144ecc85a147da
10K_Bismuth <bismuthdev@gmail.com>
2024-04-20 23:11:01 +0200

    Suite implémentation objet

--- POTO.py
+++ POTO.py
@@ -1,75 +1,110 @@
-import numpy as np
-from matplotlib import pyplot as plt
-import ot
-import cv2
-import matplotlib.pylab as pl
-import sklearn.cluster as skcluster
+import time
+
+import matplotlib.pyplot as plt
+
+from fonctions import *
 
 rng = np.random.RandomState(1)
 
 
 class POTO:
+	def __init__(self, img_ref, img_tar=None):
+		start = time.time()
 
-	def __init__(self, img_ref):
-		self.img = img_ref
-		self.matrix = self.im2mat(img_ref)
+		self.ot_model = None
+		self.img_ref = img_ref
+		self.mat_ref = im2mat(img_ref)
+		self.mat_clstr_ref = clustering(self.mat_ref)
+		end_clust_1 = time.time()
 
-	def im2mat(self, img):
-		"""
-		Convertie une image en matrice (un pixel par ligne)
-		Entrée: - img(matrix): Image que l'on cherche à transformer en matrice
-		Sortie: Matrice obtenue après le reshape
-		"""
-		return img.reshape((img.shape[0] * img.shape[1], img.shape[2]))
+		if img_tar is not None:
+			self.img_tar = img_tar
+			self.mat_tar = im2mat(img_tar)
+			self.mat_clstr_tar = clustering(self.mat_tar)
+		else:
+			self.img_tar = None
+			self.mat_tar = None
+			self.mat_clstr_tar = None
+		end_clust_2 = time.time()
 
-	def plot_distribution(self, mat, title):
-		"""
-		Affiche la distribution des couleurs de la matrice en paramètre
-		Entrées : 	- mat (matrice) : Matrice de l'image étudiée
-					- title (str) : Titre du graphe
-		"""
-		plt.scatter(mat[:, 0], mat[:, 2], c=mat)
-		plt.axis((0., 1., 0., 1.))
-		plt.xlabel('Red')
-		plt.ylabel('Blue')
-		plt.title(title)
+		print("Temps de clustering 1: ", end_clust_1 - start)
+		print("Temps de clustering 2: ", end_clust_2 - end_clust_1)
+
+	def set_target(self, img_tar):
+		self.img_tar = img_tar
+		self.mat_tar = im2mat(img_tar)
+		self.mat_clstr_tar = clustering(self.mat_tar)
+
+	def plot_distribution(self):
+		if self.mat_clstr_tar is not None:
+			figs, axs = plt.subplots(2, 1)
+			axs[0] = plot_distribution(axs[0], self.mat_clstr_ref, "Distribution de couleur de l'image de référence")
+			axs[1] = plot_distribution(axs[1], self.mat_clstr_tar, "Distribution de couleur de l'image cible")
+			plt.show()
+		else:
+			figs, axs = plt.subplots(1, 1)
+			axs[0] = plot_distribution(axs[0], self.mat_clstr_ref, "Distribution de couleur de l'image de référence")
+			plt.show()
+
+	def plot_photos(self):
+		if self.img_tar is not None:
+			figs, axs = plt.subplots(2, 1)
+			axs[1].imshow(self.img_tar)
+			axs[1].set_axis_off()
+			axs[1].set_title("Distribution de couleur de l'image cible")
+		else:
+			figs, axs = plt.subplots(1, 1)
+		axs[0].imshow(self.img_ref)
+		axs[0].set_axis_off()
+		axs[0].set_title("Distribution de couleur de l'image de référence")
 		plt.show()
 
-	def clustering(mat, model_clust):
+	def train_ot(self, method="emd"):
 		"""
-		Applique un clustering sur deux matrices d'images (Les valeurs de X doivent être entre 0 et 1).
-		Entrées :	- mat (matrice) : Matrice de la première image
-					- model_clust (model scikit learn) : Modèle de clustering
-		Sortie : - Xs (matrice) : Matrice après le clustering
+		Applique le transfert optimal sur une image X en fonction de l'algorithme choisi par l'utilisateur
+		Entrées : 	- method(str) : Modèle de transport optimal utilisé (EMD par défaut)
+		Sortie :	- modèle entraîné
 		"""
-		clust1 = model_clust.fit(mat)
-		return np.clip(clust1.cluster_centers_, 0, 1)
-
-	def train_tran_opt(Xs, Xt, method):
-		"""
-		Entraîne un modèle de transport optimal sur les données Xs et Xt.
-		Entrées : 	- Xs (matrice) : Matrice à colorer pour l'entraînement (mieux si clustering en amont)
-					- Xt (matrice) : Matrice référence pour l'entraînement (mieux si clustering en amont)
-					- method (str) : Modèle de transport optimal utilisé
-		Sortie : - ot_model (model OT) : Modèle de transport optimal entraîné
-		"""
+		start = time.time()
 		if method == "emd":
 			ot_model = ot.da.EMDTransport()
 		elif method == "sinkhorn":
 			ot_model = ot.da.SinkhornTransport(reg_e=1e-1)
 		elif method == "linear":
-			ot_model = ot.da.MappingTransport(
-				mu=1e0, eta=1e-8, bias=True, max_iter=20, verbose=True)
+			ot_model = ot.da.MappingTransport(mu=1e0, eta=1e-8, bias=True, max_iter=20, verbose=True)
 		elif method == "gaussian":
-			ot_model = ot.da.MappingTransport(
-				mu=1e0, eta=1e-2, sigma=1, bias=False, max_iter=10, verbose=True)
+			ot_model = ot.da.MappingTransport(mu=1e0, eta=1e-2, sigma=1, bias=False, max_iter=10, verbose=True)
 		else:
-			print("Les quatres choix de modèles sont : \n\
-				-'emd' : EMDTransport \n\
-				-'sinkhorn': SinkhornTransport \n\
-				-'linear': Mapping linéaire \n\
-				-'gaussian': Mapping Gaussien")
-			return None
+			raise Exception("Les quatres choix de modèles sont :\n- 'emd' : EMDTransport \n-'sinkhorn': "
+							"SinkhornTransport \n-'linear': Mapping linéaire \n-'gaussian': Mapping Gaussien")
 
-		ot_model.fit(Xs=Xs, Xt=Xt)
-		return ot_model
+		try:
+			ot_model.fit(Xs=self.mat_clstr_tar, Xt=self.mat_clstr_ref)
+			self.ot_model = ot_model
+
+			end = time.time()
+			print("Temps de color: ", end - start)
+
+			return ot_model
+		except Exception as e:
+			raise Exception("Pas de cible.")
+
+	def apply_ot(self):
+		new_img = self.ot_model.transform(Xs=self.img_tar)
+		img = minmax(mat2im(new_img, self.img_tar.shape))
+		plt.imshow(img)
+		return img
+
+
+if __name__ == '__main__':
+	path_ref = './picture_city.jpg'
+	img_ref, mat_ref = import_image(path_ref)
+
+	path_tar = './control_game_red_room.jpg'
+	img_tar, mat_tar = import_image(path_tar)
+
+	poto1 = POTO(img_ref, img_tar)
+	poto1.plot_photos()
+	# poto1.plot_distribution()
+	poto1.train_ot()
+	poto1.apply_ot()
---
+++ fonctions.py
@@ -0,0 +1,140 @@
+import cv2
+import matplotlib.pylab as pl
+import numpy as np
+import ot
+import sklearn.cluster as skcluster
+from matplotlib import pyplot as plt
+from sklearn.cluster import MiniBatchKMeans
+
+rng = np.random.RandomState(1)
+
+
+def im2mat(img):
+	"""
+    Convertie une image en matrice (un pixel par ligne)
+    Entrée : - img(matrix): Image que l'on cherche à transformer en matrice
+    Sortie : Matrice obtenue après le reshape
+    """
+	return img.reshape((img.shape[0] * img.shape[1], img.shape[2]))
+
+
+def mat2im(X, shape):
+	"""
+    Convertie une matrice en image
+    Entrées: - X(matrice): Matrice que l'on veut convertir en image
+             - shape(list): Dimensions de l'image attendue
+    Sortie: Image obtenue
+    """
+	return X.reshape(shape)
+
+
+def minmax(img):
+	"""
+    Limite les valeurs de l'image entre 0 et 1.
+    Entrée: - img(matrice): Image à modifier
+    Sortie: - np.clip(img,0,1)(matrice): Image avec des valeurs uniquement entre 0 et 1
+    """
+	return np.clip(img, 0, 1)
+
+
+def import_image(path):
+	"""
+	Importe une image et récupère sa matrice
+	Entrée: - path(str): Chemin de l'image
+	Sorties: - img(matrix): Image obtenue
+			 - mat(matrix): Matrice de l'image
+	"""
+	img = plt.imread(path).astype(np.float64) / 256
+	mat = im2mat(img)
+	return img, mat
+
+
+def print_image(ax, img, title):
+	"""
+	Affiche l'image dans l'API utilisée
+	Entrées: - img(matrix): Image à afficher
+			 - title(str): Titre de l'image
+	"""
+	ax.imshow(img)
+	ax.axis('off')
+	ax.title.set_text(title)
+	return ax
+
+
+def extract_frames(path):
+	"""
+    Récupère les images d'une vidéo
+    Entrée: - path(str): Chemin pour accéder à la vidéo
+    Sortie: - frames(list): Liste des images qui composent la vidéo
+    """
+	cap = cv2.VideoCapture(video_path)
+	frames = []
+
+	while True:
+		ret, frame = cap.read()
+		if not ret:
+			break
+		frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # récupère les images en RGB
+		frames.append(frame_rgb.astype(np.float64) / 256)
+
+	cap.release()  # libère cap
+
+	return frames
+
+
+def clustering(X, model_clust=MiniBatchKMeans(n_clusters=1000, init_size = 3000, random_state=2)):
+	"""
+    Applique un clustering sur deux matrices d'images (Les valeurs de X doivent être entre 0 et 1).
+    Entrées: - X1(matrice): Matrice de la première image
+             - model_clust(model scikit learn): Modèle de clustering
+    Sortie: - Xs(matrice): Matrice après le clustering
+    """
+	idx1 = rng.randint(X.shape[0], size=(500,))
+	clust1 = model_clust.fit(X)
+	Xs = np.clip(clust1.cluster_centers_, 0, 1)
+	return Xs
+
+
+def plot_distribution(ax, X, title):
+	"""
+	Affiche la distribution des couleurs de la matrice en paramètre
+	Entrées:
+				- ax (subplots) : Objet plot de matplotlib
+				- X(matrice): Matrice de l'image étudiée
+				- title(str): Titre du graphe
+	"""
+	ax.scatter(X[:, 0], X[:, 2], c=X)
+	ax.axis([0, 1, 0, 1])
+	ax.xlabel('Red')
+	ax.ylabel('Blue')
+	ax.title(title)
+	return ax
+
+
+def tran_opt(Xs, Xt, X, img_shape, method="emd"):
+	"""
+	Applique le transfert optimal sur une image X en fonction de l'algorithme choisi par l'utilisateur
+	Entrées: - Xs(matrice): Matrice après clustering de l'image à colorer
+			 - Xt(matrice): Matrice après clustering de l'image référence
+			 - X(matrice): Matrice de l'image à colorer
+			 - img_shape(shape): Dimension de l'image à colorer
+			 - method(str): Modèle de transport optimal utilisé (EMD par défaut)
+	Sortie: - Image recolorisée
+	"""
+	if method == "emd":
+		ot_model = ot.da.EMDTransport()
+	elif method == "sinkhorn":
+		ot_model = ot.da.SinkhornTransport(reg_e=1e-1)
+	elif method == "linear":
+		ot_model = ot.da.MappingTransport(mu=1e0, eta=1e-8, bias=True, max_iter=20, verbose=True)
+	elif method == "gaussian":
+		ot_mapping_gaussian = ot.da.MappingTransport(mu=1e0, eta=1e-2, sigma=1, bias=False, max_iter=10, verbose=True)
+	else:
+		print(
+			"Les quatres choix de modèles sont : \n- 'emd' : EMDTransport \n-'sinkhorn': SinkhornTransport \n-'linear': Mapping linéaire \n-'gaussian': Mapping Gaussien")
+		return None
+
+	ot_model.fit(Xs=Xs, Xt=Xt)
+	transp_Xs = ot_model.transform(Xs=X)
+	img = minmax(mat2im(transp_Xs, img_shape))
+	return img
