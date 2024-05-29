1944826742264edf0db34292883eb933305420d9
mgddc <95921273+mgddc@users.noreply.github.com>
2024-05-29 08:36:32 +0200

    Update ProgressBarImage.py

--- ProgressBarImage.py
+++ ProgressBarImage.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 objet : affichage d'une barre de progression
 rôle : indique le numéro de la frame sur laquelle le clustering se fait et à quelle pourcentage cela correspond
