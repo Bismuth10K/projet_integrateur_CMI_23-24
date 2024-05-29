bc2fc8b126c1269dec306831dd8dfd1aa766c9f8
mgddc <95921273+mgddc@users.noreply.github.com>
2024-05-29 08:34:41 +0200

    Add files via upload

---
+++ ProgressBarImage.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+objet : affichage d'une barre de progression
+rôle : indique le numéro de la frame sur laquelle le clustering se fait et à quelle pourcentage cela correspond
+"""
+
