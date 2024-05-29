309b6a1ca10873529a6fe5dacc8e9f94b4594f4f
mgddc <95921273+mgddc@users.noreply.github.com>
2024-05-29 08:47:19 +0200

    import tqdm

--- ProgressBarImage.py
+++ ProgressBarImage.py
@@ -1,5 +1,9 @@
 """
 objet : affichage d'une barre de progression
-rôle : indique le numéro de la frame sur laquelle le clustering se fait et à quelle pourcentage cela correspond
+rôle : indique le numéro de la frame sur laquelle le clustering se fait et à quelle pourcentage cela correspond t-il
 """
+from tqdm import tqdm
+import time
+for i in tqdm (range(10), desc="Work in progress"):
+    time.sleep(1)
 
