dadcffcd0ee952b112b73b67cd892837eec9b8b2
10K_Bismuth <bismuthdev@gmail.com>
2024-06-01 00:26:18 +0200

    Runner contient l'ensemble des codes de tests du projet

--- runner.py
+++ runner.py
@@ -1,29 +1,82 @@
-from POTV import POTV
 import time
 
-if __name__ == '__main__':
+import matplotlib
+
+from POTI import POTI
+from POTV import POTV
+from automatisation_series import automate_series
+
+
+def runner(path_imgs_ref: list, path_vid_tar: str, list_method: str = ["emd", "sinkhorn", "linear", "gaussian"]):
+	"""
+	La fonction qui automatise le traitement de plusieurs vidéos d'un coup.
+
+	Parameters
+	----------
+	path_imgs_ref (list) : Liste des chemins des images références
+	path_vid_tar (str) : Chemin de la vidéo cible
+	list_method (list) : Liste des méthodes appliquées
+	"""
+	print("Lancement en cours, ne pas s'inquiéter.")
 	start = time.time()
-	img_ref = ['./ciotat/fresque.jpg',
-			   './ciotat/marseille.jpg',
-			   './ciotat/ref_ciotat_colorise.jpg',
-			   './ciotat/VoieFerree.jpg']
-	vid_tar = './ciotat/Arrivee train à La Ciotat.mp4'
-
-	list_method = ["emd", "sinkhorn", "linear", "gaussian"]
-
 	list_model = []
-	potv1 = POTV(img_ref[0], vid_tar)
+	potv1 = POTV(path_imgs_ref[0], path_vid_tar)
 	list_model.extend([potv1.train_ot(method) for method in list_method])
 
-	for img in range(1, len(img_ref)):
-		potv1.set_reference(img_ref[img])
+	for img in range(1, len(path_imgs_ref)):
+		potv1.set_reference(path_imgs_ref[img])
 		list_model.extend([potv1.train_ot(method) for method in list_method])
 	nb_sec = round(time.time() - start, 2)
 	nb_min = round(nb_sec / 60, 2)
 	print(f"Temps d'entraînement total : {nb_sec}s, soit {nb_min}min.")
 
-	titles = ["./ciotat/render/" + img.split("/")[-1][:-4] + "_" + method for img in img_ref for method in list_method]
+	titles = ["render_" + img.split("/")[-1][:-4] + "_" + method for img in path_imgs_ref for method in list_method]
 	potv1.apply_ot(list_model, titles)
 	nb_sec = round(time.time() - start, 2)
 	nb_min = round(nb_sec / 60, 2)
 	print(f"Temps totale de render : {nb_sec}s, soit {nb_min}min.")
+
+
+if __name__ == '__main__':
+	print("Quelle version voulez-vous exécuter ?")
+	print("0 - Train à la ciotat (POTV)")
+	print("1 - Video city (POTV)")
+	print("2 - Control vers picture_city (POTI)")
+	print("3 - Cathedrale vers Control (POTI)")
+	print("4 - Séries des Cathédrales de Rouen (4 méthodes) (automatisation_series)")
+
+	match int(input("Votre choix : ")):
+		case 0:
+			img_ref = ['./ciotat/fresque.jpg', './ciotat/marseille.jpg', './ciotat/ref_ciotat_colorise.jpg',
+					   './ciotat/VoieFerree.jpg']
+			vid_tar = './ciotat/Arrivee train à La Ciotat.mp4'
+			runner(img_ref, vid_tar)
+		case 1:
+			img_ref = ['./photos/picture_city.jpg']
+			vid_tar = './videos/video_city.mp4'
+			runner(img_ref, vid_tar, ["sinkhorn"])
+		case 2:
+			img_ref = './photos/control_game_red_room.jpg'
+			img_tar = './photos/picture_city.jpg'
+
+			poti1 = POTI(img_ref, img_tar)
+			poti1.plot_photos()
+			poti1.plot_distributions()
+			poti1.train_ot()
+			matplotlib.image.imsave('control_onto_picture_city.png', poti1.apply_ot())
+		case 3:
+			img_ref = './photos/cathedrale_rouen_monet/Cathédrale de Rouen, façade ouest.jpg'
+			img_tar = './photos/control_game_red_room.jpg'
+
+			poti1 = POTI(img_ref, img_tar)
+			poti1.plot_photos()
+			poti1.plot_distributions()
+			poti1.train_ot()
+			matplotlib.image.imsave('cathedrale_onto_control.png', poti1.apply_ot())
+		case 4:
+			automate_series("./photos/cathedrale_rouen_monet", "serie_cathedrale_monet", method="emd")
+			automate_series("./photos/cathedrale_rouen_monet", "serie_cathedrale_monet", method="sinkhorn")
+			automate_series("./photos/cathedrale_rouen_monet", "serie_cathedrale_monet", method="linear")
+			automate_series("./photos/cathedrale_rouen_monet", "serie_cathedrale_monet", method="gaussian")
+		case _:
+			print("Il n'y a rien associé à ce numéro.")
